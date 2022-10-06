d8d0161c71963f35e439a9cb4319b63ef9a13209
Yash Patel <62371168+yashpatel4900@users.noreply.github.com>
2022-10-06 18:35:10 +0530

    JS Assignment 9 Done

---
+++ Javascript_Assignment_9.js
@@ -0,0 +1,19 @@
+// 1.
+
+// Yes its a closue with variable "outerVariable"
+// Output - 100
+
+// 2.
+
+// When variables are declared inside a function they are known as scope of that function
+// When a function is declared inside another function then the variable of outer functions remains accessible
+// by inner function and this is called closure.
+
+// 3.
+
+// Lexical Scope means the variable which are accessible by any function and it helps closures to carry all
+// these variables with them (inner functions accessing variables of outer function because of lexical scope)
+
+// 4.
+
+// Output = 3 3 3
